f74a3331cc45acf380847d68825dd518af45b93a
adrianosaldanhadev <adrianodev46@gmail.com>
2025-04-05 20:14:45 -0300

    Meu primeiro Commit

---
+++ scripts.js
@@ -0,0 +1,14 @@
+function generateNumber() {
+
+    const min = Math.ceil(document.querySelector('.input-min').value)
+    const max = Math.floor(document.querySelector('.input-max').value)
+
+    if (min > max) {
+       alert("Valor minino tem que ser menor que o Maximo")
+   } else {
+    const result = Math.floor(Math.random() * (max - min + 1)) + min
+    alert(result)
+   }
+
+
+}
